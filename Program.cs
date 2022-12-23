f143dc6c5b60b01999e4be907b418a829ddf9d02
yousefosman <119082963+yousefosman@users.noreply.github.com>
2022-12-23 21:04:27 +0200

    Add files via upload
    يوسف احمد عثمان علي

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static bool Isperfect(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0) sum = sum + i;
+            }
+            if (sum == number) return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("please enter n1 : ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("please enter n2 :");
+            y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n");
+            for (int i = x; i <= y; i++)
+            {
+                if (Isperfect(i)) Console.Write(" {0} ", i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
